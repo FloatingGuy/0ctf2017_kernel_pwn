79044b1b99df979c500441a0e659777cbf081ff1
lovelydream <hotdog3645@gmail.com>
2017-03-02 21:29:31 -0800

    push

--- module/note.c
+++ module/note.c
@@ -13,9 +13,64 @@
 #define LOG(...) ((void) 0)
 #endif
 
-#define BUFFER_SIZE 1024
+/* magic number */
+#define NOTE_MAGIC  0xdeadc0dedeadbeef
+#define EDIT_TIME   0xa11ab11bc11cd11d
+#define EDIT_NOTE   0x1ee12ee23ee34ee4
+
+/* knote functions */
+#define NOTE_ADD 0
+#define NOTE_DELETE 1
+#define NOTE_EDIT
+
 static bool device_inuse;
 static DEFINE_MUTEX(hlist_lock);
+static DEFINE_HASHTABLE(notes, 16);
+
+struct note_t {
+    unsigned long magic;
+    unsigned long year, month, day;
+    unsigned long h, m, s;
+    unsigned long epoch;
+    void *buf;
+    struct hlist_node next;
+};
+
+struct note_io_t {
+    unsigned long year, month, day;
+    unsigned long h, m, s;
+    void __user *buf; 
+};
+
+struct time_io_t {
+    int magic;
+    unsigned long year, month, day;
+    unsigned long h, m, s;
+};
+
+struct buf_io_t {
+    int magic;
+    void __user *buf;
+};
+    
+unsigned long inline get_epoch(struct note_t *note)
+{
+    int days[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int i;
+    unsigned long yday = 0;
+
+    if ((note->year % 4 == 0 && note->year % 100 != 0)
+       || (note->year % 400 == 0))
+        days[2] += 1
+
+    for (i = 1; i < note->month; ++i)
+        yday += days[i];
+    yday += note->day;
+
+    return note->s + note->m * 60 + note->h * 3600 + yday * 86400
+        + (note->year-70) * 31536000 + ((note->year - 69) / 4) * 86400 
+        - ((note->year - 1) / 100) * 86400 + ((note->year + 299) / 400) * 86400;
+}
 
 static void *buf;
 long test(void)
@@ -34,6 +89,11 @@ long test(void)
 	}
 
 	return 0;
+}
+
+void alloc_note(void)
+{
+    
 }
 
 static long note_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
